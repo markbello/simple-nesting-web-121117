a3bd62792bc5d77e78f553f3c62eb325065de318
Mark Bello <mcbello51286@gmail.com>
2017-11-30 22:42:24 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ programmer_hash = [:yukihiro_matsumoto => {
         }
      }
 
-		 programmer_hash = [:yukihiro_matsumoto => {
+		 programmer_hash += [:yukihiro_matsumoto => {
 		   :known_for => "Ruby",
 		    :languages => ["LISP", "C"]
 		 }]
